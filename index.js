48112829f8b4653bbc420e9714f7ad29e5d47e79
Malaka Silva <malakasilva43@gmail.com>
2023-10-07 00:20:27 +0530

    setup parcel bundler + express + react

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import express from 'express'
-const App = express();
+import express from 'express';
+import ParcelBundler from 'parcel-bundler';
 
-App.listen(8080, (err) => {
+const app = express();
+const bundler = new ParcelBundler('./public/index.html', {});
+
+const PORT = 5000;
+
+app.use(bundler.middleware());
+
+app.use(express.static('./dist'));
+
+app.get('/', function (req, res) {
+    res.sendFile('./dist/index.html');
+});
+
+app.listen(PORT, (err) => {
   if (err) console.log(err);
 
-  console.log("Server started");
+  console.log("Server is up and listening in PORT : " + PORT);
 })
---
+++ public/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+export function App () {
+
+    return (<div>Hello</div>);
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
