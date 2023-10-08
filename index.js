ff16fcc92e9b7ceba98a773b7e003b16afed4c77
Malaka Silva <malakasilva43@gmail.com>
2023-10-08 12:37:16 +0530

    replace parcel-bundler with parcel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import ParcelBundler from "parcel-bundler";
+import ParcelCore from "@parcel/core";
+const { default: Parcel } = ParcelCore;
 import bodyParser from "body-parser";
 
 import deviceAllocRouter from "./api/controller/device.allocation.controller.js";
@@ -9,7 +10,7 @@ import employeeRouter from "./api/controller/employee.controller.js";
 import userRouter from "./api/controller/user.controller.js";
 
 const app = express();
-const bundler = new ParcelBundler("./public/index.html", {});
+const bundler = new Parcel("./public/index.html", {});
 
 const PORT = 5000;
 
@@ -21,8 +22,6 @@ app.use("/api/devices", deviceRouter);
 app.use("/api/device-types", deviceTypeRouter);
 app.use("/api/employees", employeeRouter);
 app.use("/api/users", userRouter);
-
-app.use(bundler.middleware());
 
 app.use(express.static("./dist"));
 
