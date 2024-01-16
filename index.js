feb904a687bb60275a072eb8fbc24c07d95d2eed
Malaka Silva <malaka.silva@zebra.com>
2024-01-16 10:46:26 +0530

    Electron window title and size changed

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ require("./src/server/main");
 let mainWindow = null;
 
 function main() {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    title: "Asset Tracker",
+    height: 600,
+    width: 800,
+  });
   setTimeout(() => {
     mainWindow.loadURL(`http://localhost:3000/`);
   }, 5000);
   mainWindow.on("close", (event) => {
-    exec('npx kill-port 3000', (err, stdout, stderr) => {
+    exec("npx kill-port 3000", (err, stdout, stderr) => {
       console.log(err);
       console.log(stdout);
       console.log(stderr);
